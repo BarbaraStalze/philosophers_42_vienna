81439120c09bc54322cf400a918657f5c485f5e5
Barbara Stalze <bastalze@c3r2p2.42vienna.com>
2026-09-13 17:57:35 +0200

    Function that allows to sleep depending on time until end of phase

---
+++ flexsleep.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   flexsleep.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bastalze <bastalze@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/13 17:02:24 by bastalze          #+#    #+#             */
+/*   Updated: 2026/09/13 17:43:21 by bastalze         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+void	flexsleep(int64_t max_phase_length, int64_t start_of_phase)
+{
+	int64_t	curr_time;
+	int64_t	end_of_phase;
+	int64_t	remaining_time;
+
+	curr_time = get_time();
+	end_of_phase = start_of_phase + max_phase_length;
+	remaining_time = end_of_phase - curr_time;
+	if (remaining_time > 500)
+		usleep(400000);
+	else if (remaining_time > 50)
+		usleep(remaining_time * 1000 - 500);
+	else
+		usleep(100);
+}
